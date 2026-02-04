9cec8bfa138f7b7db6110f5b49646391f72fbee3
Aryan Wadher <aryan20028@gmail.com>
2026-02-04 15:24:29 -0500

    Refactored Gilded Rose using Strategy Pattern

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,41 +1,5 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -44,3 +8,89 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+# Strategy Pattern Implementation
+class UpdateStrategy:
+    def update(self, item):
+        pass
+    
+    def clamp_quality(self, item, min_val=0, max_val=50):
+        item.quality = max(min_val, min(max_val, item.quality))
+
+
+class NormalItemStrategy(UpdateStrategy):
+    def update(self, item):
+        if item.sell_in <= 0:
+            item.quality -= 2
+        else:
+            item.quality -= 1
+        item.sell_in -= 1
+        self.clamp_quality(item)
+
+
+class AgedBrieStrategy(UpdateStrategy):
+    def update(self, item):
+        if item.sell_in <= 0:
+            item.quality += 2
+        else:
+            item.quality += 1
+        item.sell_in -= 1
+        self.clamp_quality(item)
+
+
+class SulfurasStrategy(UpdateStrategy):
+    def update(self, item):
+        # Legendary item never changes
+        pass
+
+
+class BackstagePassStrategy(UpdateStrategy):
+    def update(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        else:
+            if item.sell_in <= 5:
+                item.quality += 3
+            elif item.sell_in <= 10:
+                item.quality += 2
+            else:
+                item.quality += 1
+        item.sell_in -= 1
+        self.clamp_quality(item)
+
+
+class ConjuredItemStrategy(UpdateStrategy):
+    def update(self, item):
+        if item.sell_in <= 0:
+            item.quality -= 4
+        else:
+            item.quality -= 2
+        item.sell_in -= 1
+        self.clamp_quality(item)
+
+
+# Factory Pattern
+class StrategyFactory:
+    @staticmethod
+    def get_strategy(item):
+        if item.name == "Aged Brie":
+            return AgedBrieStrategy()
+        elif "Sulfuras" in item.name:
+            return SulfurasStrategy()
+        elif "Backstage passes" in item.name:
+            return BackstagePassStrategy()
+        elif "Conjured" in item.name:
+            return ConjuredItemStrategy()
+        else:
+            return NormalItemStrategy()
+
+
+class GildedRose(object):
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            strategy = StrategyFactory.get_strategy(item)
+            strategy.update(item)
